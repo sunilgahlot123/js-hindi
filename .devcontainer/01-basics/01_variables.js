5388642e63d517780c756949afcc6017e57168a6
sunilgahlot123 <162777832+sunilgahlot123@users.noreply.github.com>
2024-03-09 08:06:02 +0000

    container config done

---
+++ .devcontainer/01-basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail ="sunil@google.com"
+var accountPassword ="123"
+accountCity ="Jaipur"
+
+
+accountEmail =  "sonu@google.com"
+accountPassword = "321"
+accountCity = "swm"
+  console.table([accountid, accountEmail, accountPassword, accountCity])
